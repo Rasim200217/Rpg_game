c23f92d175e07fd4dee79eda13a871069b23f752
unknown <rasim200217@gmail.com>
2022-02-16 13:49:24 +0500

    healthBar

---
+++ Assets/Resources/Scripts/Player/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+ public Image Healthbar;
+ public float fill;
+
+ private void Start()
+ {
+  fill = 1f;
+ }
+
+ private void Update()
+ {
+  Healthbar.fillAmount = fill;
+ }
+}
