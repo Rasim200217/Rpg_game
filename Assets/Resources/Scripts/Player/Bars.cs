f070a12228acd4b2852066f4ca3d12a1e360df54
unknown <rasim200217@gmail.com>
2022-02-17 13:48:13 +0500

    ManaBar and AgilityBar

---
+++ Assets/Resources/Scripts/Player/Bars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bars : MonoBehaviour
+{
+ public Image Healthbar;
+ public Image Manabar;
+ public Image Agilitybar;
+ [Range(0, 1f)]
+ public float fillHealth;
+ [Range(0, 1f)]
+ public float fillMana;
+ [Range(0, 1f)]
+ public float fillAgility;
+
+ private void Start()
+ {
+  fillHealth = 1f;
+  fillMana = 1f;
+  fillAgility = 1f;
+ }
+
+ private void Update()
+ {
+  Healthbar.fillAmount = fillHealth;
+  Manabar.fillAmount = fillMana;
+  Agilitybar.fillAmount = fillAgility;
+ }
+}
